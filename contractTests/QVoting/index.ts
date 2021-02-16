a1497ce8ccba6679a5e975268f87fe4fcd46c1b0
MszBednarski <msz.bednarski@gmail.com>
2021-02-16 11:13:23 +0000

    Rename tests, add more tests, one test is actually failing :test_tube: :exploding_head: :rocket:

--- contractTests/QVoting/index.ts
+++ contractTests/QVoting/index.ts
@@ -12,6 +12,11 @@ export const testQVoting: testingFunction = async (tg, code, checkerOutput, ss) 
         const optionsArr = ["opt1", "opt2"];
         const expirationTime = "100000000";
         const registrationTime = "10";
+        const emptyState = tg.genState([
+            tg.createValueParam("Map (ByStr20) (Int32)", "voter_balances", emptyArr),
+            tg.createValueParam("Map (String) (Int128)", "options_to_votes_map", emptyArr),
+            tg.createValueParam("List (ByStr20)", "registered_voters", emptyArr),
+        ], "0");
         const init = tg.genInitFromRaw([{
             "vname": "owner",
             "type": "ByStr20",
@@ -47,13 +52,13 @@ export const testQVoting: testingFunction = async (tg, code, checkerOutput, ss) 
             "type": "BNum",
             "value": registrationTime
         },
-		{
-			"vname": "token_id",
-			"type": "String",
-			"value": "ZilDOGE"
-		}]);
+        {
+            "vname": "token_id",
+            "type": "String",
+            "value": "ZilDOGE"
+        }]);
 
-        await runTest(scope,"ownerRegisterShouldRegisterWithOwnerAsSender", tg.genTestBody(
+        await runTest(scope, "ownerRegisterRegisterWithOwnerAsSender", tg.genTestBody(
             code,
             init,
             blockchain,
@@ -62,15 +67,11 @@ export const testQVoting: testingFunction = async (tg, code, checkerOutput, ss) 
                 tg.createValueParam("List (ByStr20)", "addresses", [ownerAddress, voterAddress1]),
                 tg.createValueParam("List (Int32)", "credits", ["1000", "1000"]),
             ], ownerAddress, "0"),
-            tg.genState([
-                tg.createValueParam("Map (ByStr20) (Int32)", "voter_balances", emptyArr),
-                tg.createValueParam("Map (String) (Int128)", "options_to_votes_map", emptyArr),
-                tg.createValueParam("List (ByStr20)", "registered_voters", emptyArr),
-            ], "0"),
+            emptyState,
         ),
             ss, tg);
 
-        await runTest(scope,"ownerRegisterShouldRejectWithRandomAsSender", tg.genTestBody(
+        await runTest(scope, "ownerRegisterRejectWithRandomAsSender", tg.genTestBody(
             code,
             init,
             blockchain,
@@ -79,15 +80,29 @@ export const testQVoting: testingFunction = async (tg, code, checkerOutput, ss) 
                 tg.createValueParam("List (ByStr20)", "addresses", [ownerAddress, voterAddress1]),
                 tg.createValueParam("List (Int32)", "credits", ["1000", "1000"]),
             ], voterAddress1, "0"),
-            tg.genState([
-                tg.createValueParam("Map (ByStr20) (Int32)", "voter_balances", emptyArr),
-                tg.createValueParam("Map (String) (Int128)", "options_to_votes_map", emptyArr),
-                tg.createValueParam("List (ByStr20)", "registered_voters", emptyArr),
-            ], "0"),
+            emptyState,
         ),
             ss, tg);
 
-        await runTest(scope,"voterShouldBeAbleToVoteInTime", tg.genTestBody(
+        await runTest(scope, "anyAddressRegister1", tg.genTestBody(
+            code,
+            init,
+            blockchain,
+            output,
+            tg.genMessage("register", [], voterAddress1, "0"),
+            emptyState,
+        ), ss, tg);
+
+        await runTest(scope, "anyAddressRegister2", tg.genTestBody(
+            code,
+            init,
+            blockchain,
+            output,
+            tg.genMessage("register", [], ownerAddress, "0"),
+            emptyState,
+        ), ss, tg);
+
+        await runTest(scope, "voterVoteInTime", tg.genTestBody(
             code,
             init,
             blockchain,
@@ -109,7 +124,7 @@ export const testQVoting: testingFunction = async (tg, code, checkerOutput, ss) 
         ),
             ss, tg);
 
-        await runTest(scope,"voterShouldBeRejectedAfterExpiration", tg.genTestBody(
+        await runTest(scope, "voterRejectAfterExpiration", tg.genTestBody(
             code,
             init,
             tg.genBlockchain("" + ((parseInt(expirationTime)) + 10)),
@@ -131,14 +146,14 @@ export const testQVoting: testingFunction = async (tg, code, checkerOutput, ss) 
         ),
             ss, tg);
 
-        await runTest(scope,"voteShouldAddSquaredVotes", tg.genTestBody(
+        await runTest(scope, "voterRejectBeforeRegisterPeriodEnds", tg.genTestBody(
             code,
             init,
-            blockchain,
+            tg.genBlockchain("" + (1)),
             output,
             tg.genMessage("vote", [
                 tg.createValueParam("List (String)", "options_sender", optionsArr),
-                tg.createValueParam("List (Int128)", "credits_sender", ["50", "50"]),
+                tg.createValueParam("List (Int128)", "credits_sender", ["700", "300"]),
             ], voterAddress1, "0"),
             tg.genState([
                 tg.createValueParam("Map (ByStr20) (Int32)", "voter_balances", [
@@ -147,16 +162,92 @@ export const testQVoting: testingFunction = async (tg, code, checkerOutput, ss) 
                         "val": "1000"
                     }
                 ]),
-                tg.createValueParam("Map (String) (Int128)", "options_to_votes_map", [{
-                    "key": optionsArr[0],
-                    "val": "20"
-                },
-                {
-                    "key": optionsArr[1],
-                    "val": "10"
-                }]),
+                tg.createValueParam("Map (String) (Int128)", "options_to_votes_map", emptyArr),
                 tg.createValueParam("List (ByStr20)", "registered_voters", emptyArr),
             ], "0"),
+        ),
+            ss, tg);
+
+        const preVoteMutableState = tg.genState([
+            tg.createValueParam("Map (ByStr20) (Int32)", "voter_balances", [
+                {
+                    "key": voterAddress1,
+                    "val": "1000"
+                }
+            ]),
+            tg.createValueParam("Map (String) (Int128)", "options_to_votes_map", [{
+                "key": optionsArr[0],
+                "val": "20"
+            },
+            {
+                "key": optionsArr[1],
+                "val": "10"
+            }]),
+            tg.createValueParam("List (ByStr20)", "registered_voters", emptyArr),
+        ], "0")
+
+        await runTest(scope, "voteAddSquaredVotes", tg.genTestBody(
+            code,
+            init,
+            blockchain,
+            output,
+            tg.genMessage("vote", [
+                tg.createValueParam("List (String)", "options_sender", optionsArr),
+                tg.createValueParam("List (Int128)", "credits_sender", ["50", "50"]),
+            ], voterAddress1, "0"),
+            preVoteMutableState,
+        ),
+            ss, tg);
+
+        await runTest(scope, "voteRejectIfOptionsAreDifferent", tg.genTestBody(
+            code,
+            init,
+            blockchain,
+            output,
+            tg.genMessage("vote", [
+                tg.createValueParam("List (String)", "options_sender", [...optionsArr, "something else"]),
+                tg.createValueParam("List (Int128)", "credits_sender", ["50", "50", "50"]),
+            ], voterAddress1, "0"),
+            preVoteMutableState,
+        ),
+            ss, tg);
+
+        await runTest(scope, "voteRejectIfOptionsLengthIsNotCreditLength", tg.genTestBody(
+            code,
+            init,
+            blockchain,
+            output,
+            tg.genMessage("vote", [
+                tg.createValueParam("List (String)", "options_sender", [...optionsArr, "something else"]),
+                tg.createValueParam("List (Int128)", "credits_sender", ["50", "50"]),
+            ], voterAddress1, "0"),
+            preVoteMutableState,
+        ),
+            ss, tg);
+
+        await runTest(scope, "voteRejectIfSubmittedTooManyCredits", tg.genTestBody(
+            code,
+            init,
+            blockchain,
+            output,
+            tg.genMessage("vote", [
+                tg.createValueParam("List (String)", "options_sender", optionsArr),
+                tg.createValueParam("List (Int128)", "credits_sender", ["5000", "50"]),
+            ], voterAddress1, "0"),
+            preVoteMutableState,
+        ),
+            ss, tg);
+
+        await runTest(scope, "voteAcceptNegativeCredits", tg.genTestBody(
+            code,
+            init,
+            blockchain,
+            output,
+            tg.genMessage("vote", [
+                tg.createValueParam("List (String)", "options_sender", optionsArr),
+                tg.createValueParam("List (Int128)", "credits_sender", ["-900", "100"]),
+            ], voterAddress1, "0"),
+            preVoteMutableState,
         ),
             ss, tg);
 
